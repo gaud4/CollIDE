70170b0dfc4a02fc24b2bb38fdc91f91bb2ff0bf
kh4rg0sh <36923816+MmukulKhedekar@users.noreply.github.com>
2024-04-18 16:11:04 +0530

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Input from './components/input';
 import Output from './components/output';
 import TextEditor from './components/texteditor';
+
 function App() {
   return (
     <div className="App">
