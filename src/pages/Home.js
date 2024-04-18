1ad85648fa45c0e11d0f299db94b56d4cef33a81
cruelkratos <mailgarvsethi@gmail.com>
2024-04-19 01:12:52 +0530

    home-changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import {Button, ButtonGroup} from "@nextui-org/react";
 import {v4 as uuidv4} from 'uuid';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import backgroundImage from '../image.png';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -38,6 +39,7 @@ const Home = () => {
   };
 
   return (
+    <div style={{ backgroundImage: `url(${backgroundImage})`, height: "100vh", width: "100vw"}}>
     <div style={{height: "100vh", width: "100vw", paddingLeft: "25vw", paddingRight: "25vw", paddingTop: "10vh", paddingBottom:" 10vh"}}>
       <div style={{height: "25vh", width: "50vw"}}>
         <h1 style={{fontSize: 50, textAlign: "center"}}>Welcome, to Coll-IDE</h1>
@@ -56,6 +58,7 @@ const Home = () => {
             isRequired
             type="text"
             label="Room ID"
+            variant="bordered"
             placeholder="Paste your Room ID"
             defaultValue=""
             className="max-w-xs"
@@ -76,6 +79,7 @@ const Home = () => {
             isRequired
             type="text"
             label="Username"
+            variant="bordered"
             placeholder="Username"
             defaultValue=""
             className="max-w-xs"
@@ -85,17 +89,18 @@ const Home = () => {
       </div>
       <div style={{height: "15vh", width: "50vw", display: "flex", justifyContent: "center", alignContent: "center"}}>
         <div style={{height: "15vh", width: "25vw", display: "flex", justifyContent: "center", alignContent: "center"}}>
-          <Button color="success" onClick={createNewRoom}>
+        <Button color="success" onClick={createNewRoom} style={{ backgroundColor: "white", color: "black" }}>
             Create Room
           </Button>
           {/* <a onClick={createNewRoom}>create now </a> */}
         </div>
         <div style={{height: "15vh", width: "25vw", display: "flex", justifyContent: "center", alignContent: "center"}}>
-          <Button color="success" onClick={joinRoom}>
+        <Button color="success" onClick={joinRoom} style={{ backgroundColor: "white", color: "black" }}>
             Join Room
           </Button>
         </div> 
       </div>
+    </div>
     </div>
   );
 };
