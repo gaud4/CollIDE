0fabaab83729f73fa48cec53ffd812a4a6088636
kh4rg0sh <36923816+MmukulKhedekar@users.noreply.github.com>
2024-04-18 21:08:02 +0530

    my changes

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -35,7 +35,7 @@ const EditorPage = () => {
           function handleErrors(e) {
               console.log('socket error', e);
               toast.error('Socket connection failed, try again later.');
-              reactNavigator('/');
+            //   reactNavigator('/');
           }
 
           socketRef.current.emit(ACTIONS.JOIN, {
@@ -126,27 +126,8 @@ const EditorPage = () => {
                 <label>
                     Select Language:
                     <select value={lang} onChange={(e) => { setLang(e.target.value); window.location.reload(); }} className="seLang">
-                        <option value="clike">C / C++ / C#</option>
-                        <option value="css">CSS</option>
-                        <option value="dart">Dart</option>
-                        <option value="django">Django</option>
-                        <option value="dockerfile">Dockerfile</option>
-                        <option value="go">Go</option>
-                        <option value="htmlmixed">HTML-mixed</option>
-                        <option value="javascript">JavaScript</option>
-                        <option value="jsx">JSX</option>
-                        <option value="markdown">Markdown</option>
-                        <option value="php">PHP</option>
+                        <option value="clike">GNU G++20 13.2 (64 bit)</option>
                         <option value="python">Python</option>
-                        <option value="r">R</option>
-                        <option value="rust">Rust</option>
-                        <option value="ruby">Ruby</option>
-                        <option value="sass">Sass</option>
-                        <option value="shell">Shell</option>
-                        <option value="sql">SQL</option>
-                        <option value="swift">Swift</option>
-                        <option value="xml">XML</option>
-                        <option value="yaml">yaml</option>
                     </select>
                 </label>
 
