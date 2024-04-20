604607a3d71049dd9e5b894c8ffc2d30feb15500
cruelkratos <mailgarvsethi@gmail.com>
2024-04-20 21:57:43 +0530

    server-maintain

--- server.js
+++ server.js
@@ -130,74 +130,74 @@ app.post('/submit', async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-=======
-const app = express();
+// =======
+// const app = express();
 
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
+// const http = require('http');
+// const path = require('path');
+// const { Server } = require('socket.io');
 
-const ACTIONS = require('./src/Actions');
+// const ACTIONS = require('./src/Actions');
 
-const server = http.createServer(app);
-const io = new Server(server);
-
-
-
-
-const userSocketMap = {};
-function getAllConnectedClients(roomId) {
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    );
-}
-
-
-
-io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
-
-    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-        userSocketMap[socket.id] = username;
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit(ACTIONS.JOINED, {
-                clients,
-                username,
-                socketId: socket.id,
-            });
-        });
-    });
-
-    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
-
-    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
-
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomId) => {
-            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-            });
-        });
-        delete userSocketMap[socket.id];
-        socket.leave();
-    });
-});
+// const server = http.createServer(app);
+// const io = new Server(server);
 
 
 
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// const userSocketMap = {};
+// function getAllConnectedClients(roomId) {
+//     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+//         (socketId) => {
+//             return {
+//                 socketId,
+//                 username: userSocketMap[socketId],
+//             };
+//         }
+//     );
+// }
+
+
+
+// io.on('connection', (socket) => {
+//     console.log('socket connected', socket.id);
+
+//     socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+//         userSocketMap[socket.id] = username;
+//         socket.join(roomId);
+//         const clients = getAllConnectedClients(roomId);
+//         clients.forEach(({ socketId }) => {
+//             io.to(socketId).emit(ACTIONS.JOINED, {
+//                 clients,
+//                 username,
+//                 socketId: socket.id,
+//             });
+//         });
+//     });
+
+//     socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+//         socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+//     });
+
+//     socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+//         io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+//     });
+
+//     socket.on('disconnecting', () => {
+//         const rooms = [...socket.rooms];
+//         rooms.forEach((roomId) => {
+//             socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+//                 socketId: socket.id,
+//                 username: userSocketMap[socket.id],
+//             });
+//         });
+//         delete userSocketMap[socket.id];
+//         socket.leave();
+//     });
+// });
+
+
+
+
+// const PORT = process.env.PORT || 4000;
+// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
